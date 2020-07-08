ecf1bbccb2b8c81a4f0c49c1d01555136d368a5c
Kara Cavanaugh <karacav@msn.com>
2020-07-07 20:06:20 -0400

    initial commit

---
+++ src/components/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import "./app.scss";
+
+const App: React.FC = () => {
+
+    return (
+        <div className="App">
+            MineSweeper
+        </div>
+    )
+};
+
+export default App;
